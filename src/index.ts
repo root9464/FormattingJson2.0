a149e26469bc606d0cf4fb883179c35b32472128
iO <Senpai9464@yandex.ru>
2024-08-22 17:35:14 +0500

    я заебался копировать эти json

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
-import { swapAttributesInJsonFiles } from './util/utils';
+// import { imageUrl, removeAttributes, renameAttributes } from './config/config';
 
-const jsonDir = 'D:/Рабочая область/json'; //D:/Рабочая область/json.copy
+import { findNFTsByAttributes } from './util/utils';
 
+const jsonDir1 = 'D:/Рабочая область/json'; //D:/Рабочая область/json.copy
+const jsonDir = '../json';
 // Поиск NFT по атрибутам
 // const attributes = {
-//   northern: 'northern_5', //Radiance == Less comission
-//   snow: 'snow_1', //Snowdrifts == DCA
+//   northern: 'northern_0', //Radiance == Less comission
+//   snow: 'snow_0', //Snowdrifts == DCA
 //   diamon: 'diamond_0', //Diamonds == Share or revenue
-//   arrow: 'arrow_5', //Line dots == Refferals
-//   stars: 'stars_0', //Stars == Priority
+//   arrow: 'arrow_0', //Line dots == Refferals
+//   stars: 'stars_1', //Stars == Priority
 // };
 
 const attributes = {
-  'Radiance == Less comission': '5',
+  'Radiance == Less comission': '0',
   'Snowdrifts == DCA': '1',
-  'Line dots == Refferals': '5',
-  'Stars == Priority': '0',
+  'Line dots == Refferals': '0',
+  'Stars == Priority': '1',
   'Diamonds == Share or revenue': '0',
 };
 
-swapAttributesInJsonFiles(jsonDir);
+// processJsonFiles(jsonDir1, removeAttributes, renameAttributes, imageUrl);
 
-// processJsonFiles(jsonDir, removeAttributes, renameAttributes, imageUrl);
+const nft = findNFTsByAttributes(jsonDir, attributes);
+console.log(nft);
